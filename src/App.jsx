732fafbb022cdf4291c42c0456ee03081700784c
ml8957835-son <ml8957835@gmail.com>
2026-06-22 17:26:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-6xl font-bold">
+        Deal Website
+      </h1>
+    </div>
+  );
+}
+
+export default App;
