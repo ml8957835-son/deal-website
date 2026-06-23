67ed594437c018751e1e5813510fbf54df270e34
ml8957835-son <ml8957835@gmail.com>
2026-06-23 13:07:38 +0530

    get-api-working

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,23 @@ app.get("/", (req, res) => {
   res.send("Server Running");
 });
 
+app.get("/deals", (req, res) => {
+  res.json([
+    {
+      title: "Nike Shoes",
+      store: "Nike",
+      discount: "50%",
+      description: "Summer sale"
+    },
+    {
+      title: "Dell Laptop",
+      store: "Dell",
+      discount: "20%",
+      description: "Student offer"
+    }
+  ]);
+});
+
 app.listen(5000, () => {
   console.log("Server started on port 5000");
 });
