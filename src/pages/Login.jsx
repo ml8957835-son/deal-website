28e3ec9dab7f7176b232ad9e763232fdeb4ec832
mI8957835-son <ml8957835@gmail.com>
2026-06-24 22:52:17 +0530

    user-login-working

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 
 function Login({ setIsLoggedIn }) {
   const navigate = useNavigate();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import { Link,useNavigate}  from "react-router-dom";
 
 function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleRegister = () =>
+{
+  alert("Registration Successful!");
+  navigate("/login");
+};
 
   return (
     <div className="p-6 max-w-md mx-auto">
@@ -36,6 +43,7 @@ function Register() {
       />
 
       <button
+        onClick={handleRegister}
         className="bg-blue-500 text-white px-4 py-2 rounded"
       >
         Register
